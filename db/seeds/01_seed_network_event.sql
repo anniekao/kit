d5b56042dea510690c0c0c6d4943c45d7dcbf776
Kevin Suen <kevinsuen11@gmail.com>
2019-11-04 21:41:28 -0500

    Add seeds for events

---
+++ db/seeds/01_seed_network_event.sql
@@ -0,0 +1,2 @@
+INSERT INTO network_event (id, name, location, date, start_time, end_time) VALUES
+  (1, 'Test Event', 'Toronto', '2019-11-20', '16:00', '20:00')
---
+++ db/seeds/02_seed_user_event.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_event (id, user_id, network_event_id) VALUES
+  (1, 4, 1)
