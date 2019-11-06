5f0962de0901fc47331d13aa79ff65a5dffe3ff0
Kevin Suen <kevinsuen11@gmail.com>
2019-11-05 20:01:31 -0500

    Implement get user event history route

--- routes/eventHistoryRoutes.js
+++ routes/eventHistoryRoutes.js
@@ -1,13 +1,27 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 module.exports = db => {
-  router.get("/:id/history", (req, res) => {
-    console.log(req.params.id)
+  router.get('/:id/history', async (req, res) => {
+    try {
+      const eventHistory = await db.query(
+        `
+        SELECT network_event.id, name, location, date, start_time, end_time 
+        FROM network_event
+        JOIN user_event ON network_event_id = network_event.id
+        WHERE user_id = $1 
+      `,
+        [req.params.id]
+      );
 
-    
-
-    res.status(200).json({ message: "test" })
-  })
+      if (eventHistory) {
+        res.status(200).json(eventHistory.rows);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    } catch (exception) {
+      console.error(exception);
+    }
+  });
 
   return router;
-}
+};
